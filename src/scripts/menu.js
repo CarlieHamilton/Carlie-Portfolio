94080046c09f5b2e30a2b1388c9a00160e8e77ee
BlueCodeThree <carliemhamilton@gmail.com>
2019-05-21 22:00:04 +1000

    Changed my header font

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,5 @@
+// I needed this JavaScript so that my menu would close when someone clicks on an item in the menu.
+
   const toggleInput = document.getElementById('toggle');
 
   document.querySelector('.menu').addEventListener('click', () => {
@@ -5,3 +7,11 @@
       toggleInput.checked = false;
     }
   })
+
+console.log('This is the website of Carlie Hamilton!')
+console.log('And you are looking at my source code...')
+console.log("I'm not sure if that is wise")
+console.log("But here you are.")
+console.log(":D")
+console.log("Thanks for visiting, and please contact me through my form or twitter or something, with the secret password and we can be bestest friends.")
+console.log("The secret password is: Hey Carlie, let me buy you some pizza!")
